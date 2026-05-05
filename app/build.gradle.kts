a25311fff988226ae4fe6acae61af924e7819f5d
yuya <nerentabelen@mail.ru>
2026-05-05 17:26:27 +0300

    update main activity + build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation("androidx.fragment:fragment-ktx:1.8.6")
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
@@ -56,4 +57,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
 }
--- app/src/main/java/com/yuya/currencytracker/MainActivity.kt
+++ app/src/main/java/com/yuya/currencytracker/MainActivity.kt
@@ -1,47 +1,30 @@
 package com.yuya.currencytracker
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.yuya.currencytracker.ui.theme.CurrencyTrackerTheme
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.commit
+import com.yuya.currencytracker.presentation.ui.fragments.CurrencyHistoryFragment
+import com.yuya.currencytracker.presentation.ui.fragments.CurrencyListFragment
 
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CurrencyTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                replace(R.id.fragment_container, CurrencyListFragment.newInstance())
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CurrencyTrackerTheme {
-        Greeting("Android")
+    fun openCurrencyHistory(currencyId: Int) {
+        supportFragmentManager.commit {
+            replace(
+                R.id.fragment_container,
+                CurrencyHistoryFragment.newInstance(currencyId)
+            )
+            addToBackStack(CurrencyHistoryFragment.TAG)
+        }
     }
 }

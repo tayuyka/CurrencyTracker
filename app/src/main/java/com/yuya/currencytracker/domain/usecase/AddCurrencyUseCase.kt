0ba55fa51e3cccc50d257a87b8b42735d734764f
yuya <nerentabelen@mail.ru>
2026-05-12 15:59:02 +0300

    add new history by api

--- app/src/main/java/com/yuya/currencytracker/domain/usecase/AddCurrencyUseCase.kt
+++ app/src/main/java/com/yuya/currencytracker/domain/usecase/AddCurrencyUseCase.kt
@@ -3,13 +3,14 @@ package com.yuya.currencytracker.domain.usecase
 import com.yuya.currencytracker.domain.model.Currency
 import com.yuya.currencytracker.domain.model.CurrencyIcon
 import com.yuya.currencytracker.domain.repository.CurrencyRepository
+import java.util.UUID
 import kotlin.random.Random
 
 class AddCurrencyUseCase(private val repository: CurrencyRepository) {
     operator fun invoke(code: String, name: String, icon: CurrencyIcon, isFavorite: Boolean) {
         val currencies = repository.getCurrencies()
         val newCurrency = Currency(
-            id = (currencies.maxOfOrNull { it.id } ?: 0) + 1,
+            id = UUID.randomUUID().toString(),
             code = code.uppercase(),
             name = name.ifBlank { code.uppercase() },
             currentRate = Random.nextDouble(35.0, 120.0),
